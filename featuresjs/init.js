8b111b3985e2691e3787f6e92d4f82f1eca98121
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-04-17 17:49:24 -0400

    Adding cards to jquery for transition, adding event listener

--- featuresjs/init.js
+++ featuresjs/init.js
@@ -1,7 +1,16 @@
 (function($){
   $(function(){
+    var $featurebutton = $('#buttoncolor');
+    var $card1 = $('#card1');
+    var $card1 = $('#card1');
+    var $card1 = $('#card1');
+
+    $featurebutton.on('click', function({
+      .toggleClass('scale-out');
+    }));
 
     $('.sidenav').sidenav();
-
+    
   }); // end of document ready
+
 })(jQuery); // end of jQuery name space

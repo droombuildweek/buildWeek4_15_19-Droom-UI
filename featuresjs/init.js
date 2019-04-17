72c8a9687dcc575356af9f759d4586cd62a31273
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-04-17 17:55:05 -0400

    Adjusted ID placement
    - Completed Javascript for transitions will edit if buggy

--- featuresjs/init.js
+++ featuresjs/init.js
@@ -2,12 +2,14 @@
   $(function(){
     var $featurebutton = $('#buttoncolor');
     var $card1 = $('#card1');
-    var $card1 = $('#card1');
-    var $card1 = $('#card1');
+    var $card2 = $('#card2');
+    var $card3 = $('#card3');
 
-    $featurebutton.on('click', function({
-      .toggleClass('scale-out');
-    }));
+    $featurebutton.on.click(function (){
+      $card1.toggleClass("scale-out");
+      $card2.toggleClass("scale-out");
+      $card3.toggleClass("scale-out");
+    })
 
     $('.sidenav').sidenav();
     

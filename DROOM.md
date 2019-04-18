c7f22812cd58395678f0332611568f402b99ebfd
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-04-18 08:16:26 -0400

    Restructured root folder to be much cleaner and easier to read
    - Added other pages
    - Still need to do readme
    - Cleaned up messy folder

--- DROOM.md
+++
@@ -1,2 +0,0 @@
-# buildWeek4_15_19-Droom-UI
-UI Project
---
+++ js/signupjs/init.js
@@ -0,0 +1,36 @@
+(function($){
+  $(function(){
+    var $card1 = $('#card1');
+    var $card2 = $('#card2');
+    var $card3 = $('#card3');
+
+    $('#buttoncolor').click(function (){
+      $card1.toggleClass("scale-out");
+      $card2.toggleClass("scale-out");
+      $card3.toggleClass("scale-out");
+    });
+    
+    $('#card1reveal').click(function(){
+      $card1.css('overflow', 'visible');
+      $(this).css('display', 'none');
+      $(this).css('transform', 'translateY(0%)')
+    });
+
+    $('#card2reveal').click(function(){
+      $card2.css('overflow', 'visible;');
+      $(this).css('display', 'none');
+      $(this).css('transform', 'translateY(0%)')
+    });
+
+    $('#card3reveal').click(function(){
+      $card3.css('overflow', 'visible');
+      $(this).css('display', 'none');
+      $(this).css('transform', 'translateY(0%)')
+    
+    });
+
+
+    $('.sidenav').sidenav();
+  }); // end of document ready
+
+})(jQuery); // end of jQuery name space

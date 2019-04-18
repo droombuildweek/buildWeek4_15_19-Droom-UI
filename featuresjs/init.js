930ace5f8a256c18c9b5afff0831043f99254b20
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-04-18 07:33:40 -0400

    Added variable to make code cleaner
    - Transition out of card content back to img

--- featuresjs/init.js
+++ featuresjs/init.js
@@ -1,19 +1,23 @@
 (function($){
   $(function(){
+    var $card1 = $('#card1');
+    var $card2 = $('#card1');
+    var $card3 = $('#card1');
+
     $('#buttoncolor').click(function (){
-      $('#card1').toggleClass("scale-out");
-      $('#card2').toggleClass("scale-out");
-      $('#card3').toggleClass("scale-out");
+      $card1.toggleClass("scale-out");
+      $card2.toggleClass("scale-out");
+      $card3.toggleClass("scale-out");
     });
     
     $('#card1reveal').click(function(){
-      ${''}
+      $card1.css('overflow', '');
     })
     $('#card2reveal').click(function(){
-      ${''}
+      $card2.css('overflow', '');
     })
     $('#card3reveal').click(function(){
-      ${''}
+      $card3.css('overflow', '');
     })
     $('.sidenav').sidenav();
   }); // end of document ready
